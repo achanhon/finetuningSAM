bf0d551600dbea29e1839d15ae8b2c5cea196db2
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-05-14 19:57:04 +0200

    ++

--- monfichiertest.py
+++ monfichiertest.py
@@ -1,9 +1,12 @@
 import torch
 import torchvision
+import os
 
-print("ok")
+if "build" not in os.listdir("."):
+    os.system("mkdir build")
+    os.system("cd build ; wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth")
 
-import sam_model_registry
+import segment_anything
 
-sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
+sam = segment_anything.sam_model_registry["vit_h"](checkpoint="build/sam_vit_h_4b8939.pth")
 print(sam)
