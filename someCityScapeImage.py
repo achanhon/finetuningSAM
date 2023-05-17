0f928f1d3078c6cf0417fc7161b3a074dd3df37c
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-05-17 20:48:12 +0200

    enfin

--- someCityScapeImage.py
+++ someCityScapeImage.py
@@ -37,32 +37,6 @@ data_loader = torch.utils.data.DataLoader(
 )
 
 
-def ajust_bounding_box(y, x, r, h, w):
-    # Calculate the minimum and maximum y-coordinates for the crop box
-    if y - r >= 0 and y + r < h:
-        min_y = y - r
-        max_y = y + r
-    elif y - r < 0:
-        min_y = 0
-        max_y = 2 * r
-    else:
-        max_y = h - 1
-        min_y = h - 1 - 2 * r
-
-    # Calculate the minimum and maximum x-coordinates for the crop box
-    if x - r >= 0 and x + r < w:
-        min_x = x - r
-        max_x = x + r
-    elif x - r < 0:
-        min_x = 0
-        max_x = 2 * r
-    else:
-        max_x = w - 1
-        min_x = w - 1 - 2 * r
-
-    return min_y, min_x
-
-
 def compute_value_bounding_box(image, k):
     k_indices = torch.where(image == k)
 
@@ -78,8 +52,6 @@ def compute_value_bounding_box(image, k):
 # Define a function to extract the square bounding box
 def extract_bounding_box(image, sem_labels, ins_labels):
     _, h, w = image.shape
-    print(sem_labels.shape)
-    quit()
 
     # Get the pixel coordinates of all pedestrian instances
     k = (ins_labels * (sem_labels == 24)).flatten().max()
@@ -91,16 +63,15 @@ def extract_bounding_box(image, sem_labels, ins_labels):
     ymin, xmin, ymax, xmax = compute_value_bounding_box(ins_labels, k)
 
     # Calculate the center of the bounding box
-    center_x = (xmin + xmax) // 2
-    center_y = (ymin + ymax) // 2
-
-    # Calculate the size of the square bounding box
+    x = (xmin + xmax) // 2
+    y = (ymin + ymax) // 2
     r = max(xmax - xmin, ymax - ymin) // 2
     r = min(r, 200)
 
-    left, top = ajust_bounding_box(center_y, center_x, r, h, w)
+    y = min(max(r, y), h - r - 1)-r
+    x = min(max(r, x), w - r - 1)-r
 
-    return image[:, top : top + 2 * r + 1, left : left + 2 * r + 1]
+    return image[:, y : y + 2 * r + 1, x : x + 2 * r + 1]
 
 
 # Iterate over the data loader to get batches of data
@@ -114,7 +85,9 @@ with torch.no_grad():
             image = images[i]
 
             # Extract the bounding box for the pedestrian
-            cropped_image = extract_bounding_box(image, sem_labels[i][0], ins_labels[i][0])
+            cropped_image = extract_bounding_box(
+                image, sem_labels[i][0], ins_labels[i][0]
+            )
 
             if cropped_image is not None:
                 # Resize the cropped image to 256x256 pixels
