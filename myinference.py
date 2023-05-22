db55373add1efb68cfe704891d8aebb3c6bd166e
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-05-22 15:16:13 +0200

    ++

--- myinference.py
+++ myinference.py
@@ -33,3 +33,29 @@ with torch.no_grad():
 
         out = PIL.Image.fromarray(numpy.uint8(out != 0) * 255)
         out.save("build/" + path + "_y.png")
+
+
+patch = PIL.Image.open("patchs/epoch_100_universal_patch.png")
+patch = numpy.asarray(patch.convert("RGB").copy())
+with torch.no_grad():
+    for path in paths:
+        image = PIL.Image.open("cityscapesample/" + path)
+        image = numpy.asarray(image.convert("RGB").copy())
+        image[0:110,0:110,:]=patch
+        tmp = PIL.Image.fromarray(image)
+        tmp.save("build/" + path + "_a.png")
+        image = numpy.transpose(image, axes=(2, 0, 1))
+
+        x = {}
+        x["image"] = torch.Tensor(image).cuda()
+        x["original_size"] = (256, 256)
+        x["point_coords"] = torch.ones(1, 1, 2).cuda() * 128
+        x["point_labels"] = torch.ones(1, 1).cuda()
+
+        out = sam([x], True)[0]
+        out = out["masks"][0]
+        out,_ = out.max(0)
+        out = out.float().cpu().numpy()
+
+        out = PIL.Image.fromarray(numpy.uint8(out != 0) * 255)
+        out.save("build/" + path + "_b.png")
