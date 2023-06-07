925842173ec70e08fa74670e1705488e4f12b628
moi_moi_et_moi <adrien.chan_hon_tong@onera.fr>
2023-06-07 09:50:06 +0200

    ++

--- monfichiertest.py
+++ monfichiertest.py
@@ -71,7 +71,6 @@ for i, (row, col) in enumerate(tmp):
 
 with torch.no_grad():
     tmp = PIL.Image.open("/scratchf/miniworld/potsdam/train/0_x.png")
-    tmp.save("build/x.png")
 
     tmp = numpy.asarray(tmp.convert("RGB").copy())
     h, w, c = tmp.shape
@@ -93,3 +92,42 @@ with torch.no_grad():
     for i in range(magrille.shape[0]):
         tmp = PIL.Image.fromarray(numpy.uint8(out[i] != 0) * 255)
         tmp.save("build/y" + str(i) + ".png")
+
+import os
+
+os.system("rm build/x*")
+os.system("rm build/y*")
+
+print("wtf")
+tmp = []
+for row in range(6, 255, 12):
+    for col in range(6, 255, 12):
+        tmp.append((row, col))
+magrille = torch.zeros(len(tmp), 1, 2).cuda()
+magrilleL = torch.zeros(len(tmp), 1).cuda()
+for i, (row, col) in enumerate(tmp):
+    magrille[i][0][0] = row
+    magrille[i][0][1] = col
+    magrilleL[i][0] = i
+
+with torch.no_grad():
+    tmp = PIL.Image.open("build/grosse.png")
+    tmpIMAGE = tmp.resize((256, 256))
+
+    tmp = numpy.asarray(tmpIMAGE.convert("RGB").copy())
+    tmp = torch.Tensor(numpy.transpose(tmp, axes=(2, 0, 1)))
+
+    x = {}
+    x["image"] = tmp.cuda()
+    x["original_size"] = (256, 256)
+    x["point_coords"] = magrille
+    x["point_labels"] = magrilleL
+
+    out = sam([x], False)[0]["masks"]
+    out, _ = out.max(1)
+    out = out.cpu().numpy()
+
+    for i in range(magrille.shape[0]):
+        tmp = PIL.Image.fromarray(numpy.uint8(out[i] != 0) * 255)
+        tmpIMAGE.save("build/" + str(i) + "x.png")
+        tmp.save("build/" + str(i) + "y.png")
